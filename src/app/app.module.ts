a4bf849c651a160d992e9a4b8253fb7d555a13a9
KaneD97 <kanedharan@gmail.com>
2021-02-01 17:27:02 +0000

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,6 @@ import { HomeComponent } from "./home.component";
     ReviewsComponent,
     HomeComponent
   ],
-  bootstrap: [AppComponent, HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
