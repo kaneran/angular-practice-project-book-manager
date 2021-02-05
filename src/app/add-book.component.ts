0cb3cc20d9802ab428695544f395ef833c0595f7
KaneD97 <kanedharan@gmail.com>
2021-02-05 17:29:19 +0000

    Added book form

--- src/app/add-book.component.ts
+++ src/app/add-book.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  templateUrl: "./add-book.component.html",
   styles: [`h1 { font-family: Lato; }`]
 })
 export class AddBookComponent  {
